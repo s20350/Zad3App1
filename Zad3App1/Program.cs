cd96a109072c86694f9eb18baa33723881da389a
s20350 <s20350@pjwstk.edu.pl>
2024-04-25 22:45:26 +0200

    abc

--- Zad3App1/Program.cs
+++ Zad3App1/Program.cs
@@ -4,14 +4,12 @@ using Zad3App1.Model;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AnimalContext>(opt => 
     opt.UseSqlite(builder.Configuration.GetConnectionString("AnimalContext")));
 
 var app = builder.Build();
 
-// Initialize and seed the database
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
