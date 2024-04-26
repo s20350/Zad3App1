e573b8bd213ee1d92c37de57b43ff367278e5b46
s20350 <s20350@pjwstk.edu.pl>
2024-04-26 19:30:31 +0200

    Corrections

--- Zad3App1/Controllers/AnimalController.cs
+++ Zad3App1/Controllers/AnimalController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Zad3App1.Data;
 using Zad3App1.Model;
+using System.Linq;
 
 namespace Zad3App1.Controllers;
 
@@ -17,9 +18,28 @@ public class AnimalController : ControllerBase
     }
     
     [HttpGet]
-    public async Task<IActionResult> GetAnimals()
+    public async Task<IActionResult> GetAnimals([FromQuery] string orderBy = "name")
     {
-        var animals = await _context.Animals.ToListAsync();
+        IQueryable<Animal> query = _context.Animals;
+
+        switch (orderBy.ToLower())
+        {
+            case "description":
+                query = query.OrderBy(a => a.Description);
+                break;
+            case "category":
+                query = query.OrderBy(a => a.Category);
+                break;
+            case "area":
+                query = query.OrderBy(a => a.Area);
+                break;
+            case "name":
+            default:
+                query = query.OrderBy(a => a.Name);
+                break;
+        }
+
+        var animals = await query.ToListAsync();
         return Ok(animals);
     }
     
@@ -28,6 +48,68 @@ public class AnimalController : ControllerBase
     {
         _context.Animals.Add(animal);
         await _context.SaveChangesAsync();
-        return CreatedAtAction("GetAnimals", new { id = animal.Id }, animal);
+        return CreatedAtAction(nameof(GetAnimal), new { id = animal.Id }, animal);
+    }
+    
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Animal>> GetAnimal(int id)
+    {
+        var animal = await _context.Animals.FindAsync(id);
+
+        if (animal == null)
+        {
+            return NotFound();
+        }
+
+        return animal;
+    }
+    
+    [HttpPut("{idAnimal}")]
+    public async Task<IActionResult> PutAnimal(int idAnimal, [FromBody] Animal animal)
+    {
+        if (idAnimal != animal.Id)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(animal).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!AnimalExists(idAnimal))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+    
+    [HttpDelete("{idAnimal}")]
+    public async Task<IActionResult> DeleteAnimal(int idAnimal)
+    {
+        var animal = await _context.Animals.FindAsync(idAnimal);
+        if (animal == null)
+        {
+            return NotFound();
+        }
+
+        _context.Animals.Remove(animal);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private bool AnimalExists(int id)
+    {
+        return _context.Animals.Any(e => e.Id == id);
     }
 }
--- Zad3App1/Model/Animal.cs
+++ Zad3App1/Model/Animal.cs
@@ -1,12 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Zad3App1.Model
 {
     
     public class Animal
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
         public string Name { get; set; }
+
+        [StringLength(500)]
         public string Description { get; set; }
+
+        [Required]
         public string Category { get; set; }
+
+        [Required]
         public string Area { get; set; }
     }
 
